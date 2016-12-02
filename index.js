9dde04ad716e2709a8636f5c2ccd932643fac93b
khoskins <katiehoskins18@gmail.com>
2016-12-02 17:10:49 -0500

    heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 //app.use("/styles",  express.static(__dirname + '/public/stylesheets'));
 //app.use("/scripts", express.static(__dirname + '/public/javascripts'));
 app.use("/img",  express.static(__dirname + '/img'));
